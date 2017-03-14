1554bee101f8a046cc4352dc1151d3474f872e23
rajas <rajas.bhalerao@gmail.com>
2017-03-14 16:18:16 +0530

    product links added

--- app/src/main/java/com/goncalojoaocorreia/cameraruler/Main3Activity.java
+++ app/src/main/java/com/goncalojoaocorreia/cameraruler/Main3Activity.java
@@ -2,12 +2,18 @@ package com.goncalojoaocorreia.cameraruler;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class Main3Activity extends ActionBarActivity {
-
+    WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        webView = (WebView) findViewById(R.id.webView1);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://ubiquitoushomes.com/");
+
     }
 }
