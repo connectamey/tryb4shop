6771dfe200492d10201da7101d091c435dd71f62
coolamz <aabansod@gmail.com>
2017-03-27 17:30:00 +0530

    new

--- app/src/main/java/com/be/ubihomes/Main3Activity.java
+++ app/src/main/java/com/be/ubihomes/Main3Activity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class Main3Activity extends ActionBarActivity {
     WebView webView;
@@ -14,6 +15,13 @@ public class Main3Activity extends ActionBarActivity {
         webView = (WebView) findViewById(R.id.webView1);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("https://ubiquitoushomes.com/");
+
+        Bundle bundle = getIntent().getExtras();
+        String name = bundle.getString("NAME");
+        String type_length = bundle.getString("LEN");
+        String type_height = bundle.getString("HEI");
+
+        Toast.makeText(Main3Activity.this, "Category: "+name+"\n Length: "+type_length+"\n Height: "+type_height,Toast.LENGTH_SHORT).show();
 
     }
 }
