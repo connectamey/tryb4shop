2ba9335a4d83a26383f3f4fb20ef046244568533
coolamz <aabansod@gmail.com>
2017-05-29 16:52:51 +0530

    only bg camera remaining

--- app/src/main/java/com/be/ubihomes/Main3Activity.java
+++ app/src/main/java/com/be/ubihomes/Main3Activity.java
@@ -149,37 +149,6 @@ public class Main3Activity extends ActionBarActivity {
 
             }
         });
-//        BroadcastReceiver receiver = new BroadcastReceiver() {
-//            @Override
-//            public void onReceive(Context context, Intent intent) {
-//                String action = intent.getAction();
-//                if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)) {
-//                    long downloadId = intent.getLongExtra(
-//                            DownloadManager.EXTRA_DOWNLOAD_ID, 0);
-//                    DownloadManager.Query query = new DownloadManager.Query();
-//                    query.setFilterById(enqueue);
-//                    Cursor c = dm.query(query);
-//                    if (c.moveToFirst()) {
-//                        int columnIndex = c
-//                                .getColumnIndex(DownloadManager.COLUMN_STATUS);
-//                        if (DownloadManager.STATUS_SUCCESSFUL == c
-//                                .getInt(columnIndex)) {
-//
-//                            //ImageView view = (ImageView) findViewById(R.id.imageView1);
-//                            String uriString = c
-//                                    .getString(c
-//                                            .getColumnIndex(DownloadManager.COLUMN_LOCAL_URI));
-//                           // view.setImageURI(Uri.parse(uriString));
-//                        }
-//                    }
-//                }
-//            }
-//        };
-//
-//        registerReceiver(receiver, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
-//
-//        //Toast.makeText(this, "Downloaded", Toast.LENGTH_SHORT).show();
-
 
 
         progressBar = ((ProgressBar) findViewById(R.id.progress_loader));
